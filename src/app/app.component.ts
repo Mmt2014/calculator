bbc84a2bcfa14da3051a18d41db2c466719b9c14
Murli <murlimanohartripathy1997@gmail.com>
2022-12-06 13:47:31 +0530

    1st commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormControl,FormGroup} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'mycalculator';
+  toshow ='0'
+  currvalue=''
+writetoinput(value: string){
+  this.currvalue = this.currvalue + value
+  this.toshow = this.currvalue
+}
+equalto(){
+  this.toshow=eval(this.currvalue)
+  this.currvalue = this.toshow
+}
+clear(){
+  this.currvalue='';
+  this.toshow = '0'
+}
+
+}
